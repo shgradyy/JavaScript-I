17d29c2f7a6cbbf66a14ec6c21fae9f7884e773e
Shantay Grady <shantaygrady@gmail.com>
2019-05-22 22:40:38 -0400

    Completes Objects challenges

--- assignments/objects.js
+++ assignments/objects.js
@@ -10,12 +10,12 @@
 // 5,adaine5@samsung.com,Antonietta,F
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
-const example = {
+/*const example = {
   "id": 0,
   "name": "Example",
   "email": "examples@you.edu",
   "gender": "F"
-}
+} */
 
 // Write your intern objects here:
 const intern1 = {
@@ -23,53 +23,58 @@ const intern1 = {
   "email": "mmelloy0@psu.edu",
   "firstName": "Mitzi",
   "gender": "F"
-},
+}
 const intern2 = {
   "id": 2,
   "email": "kdiben1@tinypic.com",
   "firstName": "Kennan",
   "gender": "M"
-},
+}
 const intern3 = {
   "id": 3,
   "email": "kmummery2@wikimedia.org",
   "firstName": "Keven",
   "gender": "M"
-},
+}
 const intern4 = {
   "id": 4,
   "email": "gmartinson3@illinois.edu",
   "firstName": "Gannie",
   "gender": "M"
-},
+}
 const intern5 = {
   "id": 5,
   "email": "adaine5@samsung.com",
   "firstName": "Antonietta",
   "gender": "F"
-}
+};
 
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(intern1.firstName);
 // Kennan's ID
-
+console.log(intern2.id);
 // Keven's email
-
+console.log(intern3.email);
 // Gannie's name
-
+console.log(intern4.firstName);
 // Antonietta's Gender
+console.log(intern5.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-
+intern2.speak = "Hello, my name is Kennan!";
+console.log(intern2.speak); //test
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-
+intern5.multiplyNums = function(a, b) {
+  return a + b;
+}
+console.log(intern5.multiplyNums(5,6));
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
